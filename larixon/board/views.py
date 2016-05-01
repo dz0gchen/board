eeff0a6cc3d03694dd39fd3c196c1e91dc926205
strawposter <strawposter@mail.ru>
2016-05-01 17:25:00 +0300

    counter

--- larixon/board/views.py
+++ larixon/board/views.py
@@ -1,20 +1,56 @@
 #coding: utf-8
 
+import os
+import re
 from .models import Advert
 from django.views.generic import ListView, DetailView
+
+#c:\Program Files\Redis>redis-server --service-start redis.windows-service.conf --sentinel
+import redis
+from django.conf import settings
+cache = redis.StrictRedis(host=settings.REDIS_HOST,
+                          port=settings.REDIS_PORT,
+                          db=settings.REDIS_DB)
 
 class Index(ListView):    
     model = Advert
     paginate_by = 2
     context_object_name = 'index'   
     template_name = 'index.html'
-
+    
 #детализация объявления
 class Detail(DetailView):
     model = Advert
     context_object_name = 'detail'
     template_name = 'detail.html'
-
+    
+    def dispatch(self, request, *args, **kwargs):
+                 
+        pattern =  r'\d+'
+        url = request.get_full_path()
+        result = re.search(pattern, url)
+        id = result.group(0)
+        
+        key = str(request.META['HTTP_USER_AGENT'] + request.META['REMOTE_ADDR'] + id)
+        #cache.flushall()
+        
+        #если ключ уже существует ничего не делаем
+        if cache.get(key):
+            pass
+        else:
+            #добавляем ключ, значение ключа не играет роли
+            cache.append(key, id)            
+            #ключ будет жить в хранилище сутки
+            cache.expire(key, 86400)
+            #если для id уже ведется счетчик - инкрементируем
+            if cache.get(id):
+                cache.incr(id)
+            else:
+                #создаем счетчик для id
+                cache.append(id, 1)
+                 
+        return super(Detail, self).dispatch(request, *args, **kwargs)
+    
     def get_object(self):
         obj = super(Detail, self).get_object()
         return obj
--- larixon/larixon/settings.py
+++ larixon/larixon/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 1
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
